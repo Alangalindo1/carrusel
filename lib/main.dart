26d56258b701908311dddb011467382bbe788335
ALAN_OSVALDO_GALINDO_ACEVEDO <a.21308051280475@cbtis128.edu.mx>
2024-08-12 14:03:19 +0000

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'hola carrusel',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // A widget which will be started on application startup
+        home: const CarruselPeros());
   }
-}
+} //fin clase myapp
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class CarruselPeros extends StatefulWidget {
+  const CarruselPeros({Key? key}) : super(key: key);
 
   @override
+  State<CarruselPeros> createState() => _CarruselPerosState();
+} //fin clase carrusel perros stateful
+
+class _CarruselPerosState extends State<CarruselPeros> {
+  late PageController _pageController;
+  List<String> images = [
+    "https://raw.githubusercontent.com/Alangalindo1/LitsviewMacota/main/perros/perro1.jpg",
+    "https://raw.githubusercontent.com/Alangalindo1/LitsviewMacota/main/perros/perro2.jpg",
+    "https://raw.githubusercontent.com/Alangalindo1/LitsviewMacota/main/perros/perro3.jpg"
+  ];
+
+  int activePage = 1;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+    return PageView.builder(
+        itemCount: 2,
+        pageSnapping: true,
+        itemBuilder: (context, pagePosition) {
+          return Container(
+              margin: EdgeInsets.all(10),
+              child: Image.network(images[pagePosition]));
+        });
   }
-}
+} //fin clase estado
